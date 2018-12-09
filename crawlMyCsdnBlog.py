30f92335a9a76c480b08c4755fab18fa4628f461
ryanliang000 <mail>
2018-12-10 00:23:06 +0800

    support download img file to local

--- crawlMyCsdnBlog.py
+++ crawlMyCsdnBlog.py
@@ -3,6 +3,9 @@ from requests_html import HTMLSession
 import os
 import sys
 import re
+import mypub
+
+session = HTMLSession()
 
 #get input username
 userName = input("Please inpu csdn username:")
@@ -21,20 +24,41 @@ outputDir = userName
 if not os.path.exists(outputDir):
     os.mkdir(outputDir)
 
+imgDir = outputDir + "/" + "img"
+if not os.path.exists(imgDir):
+    os.mkdir(imgDir)
+
+
+#save img file to local directory and return the replace html text
+def getExchangedText(contentElement):
+    htmlText = contentElement.html
+    for img in contentElement.find("img"):
+        #mypub.printVar(img.attrs["src"])
+        try:
+            url = img.attrs["src"]
+            imgName = url[url.rindex("/") + 1:]
+            page = session.get(url)
+            page.raise_for_status()
+            localAddr = imgDir + "/" + imgName
+            mypub.writeBinFile(localAddr, page.content)
+            #mypub.printVar(localAddr)
+            htmlText = htmlText.replace(url, "img/" + imgName)
+        except:
+            continue
+    return htmlText
 #write content to html file
 def writeHtmlContent(fileName, titleElement, contentElement):
-    file = open(fileName, "w", encoding="utf-8")
-    file.write('<html lang="zh-CN">\r\n')
-    file.write('  <head>\r\n')
-    file.write('    <meta http-equiv="content-type" content="text/html; charset=utf-8">\r\n')
-    file.write('    <title>{}</title>\r\n'.format(titleElement.text))
-    file.write('  </head>\r\n')
-    file.write('  <body>\r\n')
-    file.write('    <div class="article-title-box">{}</div>\r\n'.format(titleElement.html))
-    file.write(contentElement.html)
-    file.write('\r\n  </body>\r\n')
-    file.write('</html>\r\n')
-    file.close()
+    c  = '<html lang="zh-CN">\r\n'
+    c += '  <head>\r\n'
+    c += '    <meta http-equiv="content-type" content="text/html; charset=utf-8">\r\n'
+    c += '    <title>{}</title>\r\n'.format(titleElement.text)
+    c += '  </head>\r\n'
+    c += '  <body>\r\n'
+    c += ('    <div class="article-title-box">{}</div>\r\n'.format(titleElement.html))
+    c += getExchangedText(contentElement)
+    c += '\r\n  </body>\r\n'
+    c += '</html>\r\n'
+    mypub.writeTextFile(fileName, c)
 
 #repalce specail charactor
 def getValidFileName(fileName):
@@ -42,7 +66,6 @@ def getValidFileName(fileName):
     fileName = re.sub('__+', "_", fileName)              #replace duplicate '_'
     return fileName
 
-session = HTMLSession()
 mainUrl = "https://blog.csdn.net/{}/".format(userName)
 mainPage = session.get(mainUrl)
 if (isPageError(mainPage)):
@@ -51,7 +74,7 @@ if (isPageError(mainPage)):
     
 #default max get 100 pages
 checkUrlUserName= "/" + userName + "/"
-for i in range(1, 101):
+for i in range(13, 101):
     listPage = session.get(mainUrl + "article/list/" + str(i))
     articleUrlList = listPage.html.find("#mainBox > main > div.article-list", first=True)
     if (articleUrlList is None):
@@ -74,7 +97,8 @@ for i in range(1, 101):
         title = titleElement.text
         title = getValidFileName(title)
         print("article: {}".format(title))
-        htmlFileName = "{}\\Page{}_{}.html".format(outputDir, i, title);
+        htmlFileName = os.path.abspath("{}/Page{}_{}.html".format(outputDir, i, title));
+        #mypub.outputVar(htmlFileName)
         contentElement = page.html.find("#content_views", first=True)
         writeHtmlContent(htmlFileName, titleElement, contentElement)
  
